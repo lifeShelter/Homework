30b08f17456192fb032a09fc3a07a319636bd16d
Life <wjsxn7@gmail.com>
2021-04-23 18:18:27 +0900

    - 검색 이력 저장 기능 추가(검색어가 안겹치게)

--- Homework/Homework/ViewModel/SearchListViewModel.swift
+++ Homework/Homework/ViewModel/SearchListViewModel.swift
@@ -24,10 +24,26 @@ class SearchListViewModel {
     
     init() {
         tapSearchButton.withLatestFrom(searchBarText)
-            .filter { $0.trimmingCharacters(in: .whitespacesAndNewlines) != ""}
-            .subscribe(onNext:{ str in
+            .map { $0.trimmingCharacters(in: .whitespacesAndNewlines)}
+            .filter { $0 != ""}
+            .subscribe(onNext:{[weak self] str in
                 print("sub")
                 print(str)
+                self?.addHistory(str)
             }).disposed(by: disposeBag)
+    }
+    
+    
+    private func addHistory(_ str:String) {
+        if let history = UserDefaults.standard.array(forKey: "history") as? Array<String> {
+            var historySet:Set<String> = Set(history.map {$0})
+            historySet.insert(str)
+            print("historySet = \(historySet)")
+            UserDefaults.standard.setValue(Array(historySet), forKey: "history")
+            UserDefaults.standard.synchronize()
+        } else {
+            UserDefaults.standard.setValue( [str], forKey: "history")
+            UserDefaults.standard.synchronize()
+        }
     }
 }
