44f7966e31322dd3f27da70b67d9ce2326f87b87
Life <wjsxn7@gmail.com>
2021-04-23 16:24:24 +0900

    - rxswift및 필요 라이브러리 빌드 확인 - scene delegate 제거

---
+++ Homework/Homework/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Homework
+//
+//  Created by 허원재 on 2021/04/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
